5fa45d2487e1b1172143bc31e7f78dcc9077c6ca
nurulhikmahaulia <nurul.hikmah.aulia17@gmail.com>
2024-08-26 09:45:20 +0700

    update

---
+++ main.js
@@ -0,0 +1,27 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
+import { 
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  getDoc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  query,
+  orderBy
+  } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgSS-chZUH5T47nhRNeK6jYDnGZK_TQSA",
+  authDomain: "insan-cemerlang-d6eb1.firebaseapp.com",
+  projectId: "insan-cemerlang-d6eb1",
+  storageBucket: "insan-cemerlang-d6eb1.appspot.com",
+  messagingSenderId: "162904381844",
+  appId: "1:162904381844:web:dd88782fdcc494c9ac1781",
+  measurementId: "G-1RSX6TCWZ2"
+};
+
+// Inisialisasi Firebase
+const aplikasi = initializeApp(firebaseConfig)
+const basisdata = getFirestore(aplikasi)
